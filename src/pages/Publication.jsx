5939b3d60947a9c25d1bc37fd853e3a078ed00e7
shelcia <shelcia1999@gmail.com>
2021-01-11 02:51:23 +0530

    update

--- src/pages/Publication.jsx
+++ src/pages/Publication.jsx
@@ -32,7 +32,7 @@ const Publication = () => {
               <div className="card-body">
                 <h4 className="card-title">{pub.title}</h4>
                 <h5 className="mb-2 text-muted card-subtitle">
-                  <span>Contributors: </span>
+                  {/* <span>Contributors: </span> */}
                   {pub.contributor.map((cont, index) => (
                     <em key={index + 1}>
                       {cont}
@@ -41,11 +41,32 @@ const Publication = () => {
                   ))}
                 </h5>
                 <p className="card-text">
-                  <em>Name of the publisher</em>
+                  <em>{pub.desc}</em>
                 </p>
-                <a className="card-text" href={pub.doi}>
-                  {pub.doi}
-                </a>
+                <div className="w-100">
+                  {pub.volume && (
+                    <p className="card-text mr-4 d-inline-block">
+                      <i className="fas fa-book pr-2 pb-0" /> <em>Volume: </em>
+                      {pub.volume}
+                    </p>
+                  )}
+                  {pub.pages && (
+                    <p className="card-text mr-4 d-inline-block">
+                      <i className="fas fa-book-open pr-2 pb-0" />
+                      <em>Pages: </em> {pub.pages}
+                    </p>
+                  )}
+                  <p className="card-text mr-4 d-inline-block">
+                    <i className="fas fa-calendar-alt pr-2 pb-0" />
+                    <em>Year: </em>
+                    {pub.year}
+                  </p>
+                </div>
+                <div className="mt-2">
+                  <a className="card-text" href={pub.doi} target="blank">
+                    {pub.doi}
+                  </a>
+                </div>
               </div>
             </div>
           ))
